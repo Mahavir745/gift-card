d6ce81355529f9576cebfba559592f89333787ef
Mahavir745 <mahavir777kumar@gmail.com>
2025-01-08 07:52:51 +0530

    update ui

--- src/component/Home/CardFrame.jsx
+++ src/component/Home/CardFrame.jsx
@@ -1,6 +1,7 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { BiDownload } from 'react-icons/bi';
 import { toJpeg } from 'html-to-image';
+import dancing_font from "../../font/DancingScript.ttf"
 
 const CardFrame = ({ item, name, heading, image }) => {
   const cardRef = useRef();
@@ -8,10 +9,15 @@ const CardFrame = ({ item, name, heading, image }) => {
   async function handleDownload () {
     if (cardRef.current) {
       try {
-        const dataUrl = await toJpeg(cardRef.current);
+        const dataUrl = await toJpeg(cardRef.current ,{
+          canvasWidth: 375,
+          style:{
+            margin: "0",
+          }
+        });
         const link = document.createElement('a');
         link.href = dataUrl;
-        link.download = `${heading}-card.png`;
+        link.download = `${heading}-card.jpeg`;
         link.click();
       } catch (error) {
         console.error('Error downloading the card:', error);
@@ -23,9 +29,9 @@ const CardFrame = ({ item, name, heading, image }) => {
     <div className='flex justify-between'>
       <div
         ref={cardRef}
-        className='w-[400px] sm:w-[400px] h-[500px] border shadow-lg m-4 rounded-lg p-2 flex flex-col justify-evenly bg-white'
+        className='w-[375px] sm:w-[400px] h-[500px] border shadow-lg m-4 rounded-lg p-2 flex flex-col justify-evenly bg-white'
       >
-        <h1 className='text-5xl fontfamily'>{heading}</h1>
+        <h1 className='text-5xl text-center fontfamily'>{heading}</h1>
         <img src={image} alt="" className='w-[100%] h-[200px]' />
         <p className='text-center text-[20px]'>{item}</p>
         <h3 className='text-center text-[50px] fontfamily'>{name}</h3>
